5bed78a475e40f5af36ffe86ae7dca88bcca3b3d
chul1631 <sonky201@gmail.com>
2022-09-30 11:13:29 +0900

    pjt

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    
+    return render(request,"app/index.html")
+
+def new(request):
+    
+    return render(request,"app/new.html")
+    
+    
